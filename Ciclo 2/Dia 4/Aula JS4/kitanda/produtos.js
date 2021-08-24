8a7a401bf70075ebdbe47a4767315bd32061e431
WillianHT <88731001+WillianHT@users.noreply.github.com>
2021-08-24 20:44:49 -0300

    Create produtos.js

---
+++ Ciclo 2/Dia 4/Aula JS4/kitanda/produtos.js
@@ -0,0 +1,7 @@
+var produtos = [
+	{proDesc: "Mamão Papaia", prodPreco: 4.70},
+	{proDesc: "Laranja", prodPreco: 3.70},
+	{proDesc: "Manga", prodPreco: 2.70},
+    {prodDesc: "Melão", prodPreco : 6.90 },
+    {prodDesc: "Melancia", prodPreco : 5.00 },
+]
